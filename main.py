c504ee2b3064ae22320dc100e1be784ed699ad89
motive <motive@mathflat.com>
2021-08-05 23:35:57 +0900

    changed

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ def helloworld(a):
     msg = f"Hello World! {a}"
     print(msg)  # hmm
     return msg
-
 def helloworld(a):
     msg = f"Hello World! {a}"
     print(msg)  # hmm
     return msg
-
-
 if __name__ == "__main__":  # pragma: no cover
     helloworld("Chris")
